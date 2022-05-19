3c87b503618dbf4ce265dda443d9acdeaccfd115
David Wei <david94.wei@gmail.com>
2022-05-19 17:01:11 +0200

    est.trafo_est: Add TrafoEstimator

--- state_reconstruction/est/trafo_est.py
+++ state_reconstruction/est/trafo_est.py
@@ -9,6 +9,7 @@ import numpy as np
 import scipy.optimize
 import time
 
+from libics.env.logging import get_logger
 from libics.core.data.arrays import ArrayData
 from libics.core.util import misc
 from libics.tools.trafo.linear import AffineTrafo2d
@@ -24,7 +25,8 @@ from libics.tools.math.peaked import FitGaussian1d, gaussian_1d, FitParabolic1d
 
 def get_trafo_angle_fidelity(
     x, y, guess_trafo, ax=(0, 1),
-    bins_per_site=5, peak_rel_prominence=0.1, peak_base_prominence_ratio=0.1
+    bins_per_site=5, peak_rel_prominence=0.1, peak_base_prominence_ratio=0.1,
+    rel_spacing_tol=0.2
 ):
     # Parse parameters
     ax_is_scalar = np.isscalar(ax)
@@ -38,6 +40,7 @@ def get_trafo_angle_fidelity(
     proj_dists = []
     spacings = []
     widths = []
+    fidelities = []
     for _sites in sites[ax_mask]:
         _dsites = _sites - _sites[:, np.newaxis]
         # Filter for positive distances (symmetric) and
@@ -58,29 +61,55 @@ def get_trafo_angle_fidelity(
         _peaks = find_peaks_1d(
             _c, _h, ret_vals=["width"],
             rel_prominence=peak_rel_prominence,
-            base_prominence_ratio=peak_base_prominence_ratio
+            base_prominence_ratio=peak_base_prominence_ratio,
+            fit_algorithm="mean"
         )
         # Find spacing from peaks
         _peak_pos = np.array(_peaks["center"])
         _dpeak_pos = (_peak_pos - _peak_pos[:, np.newaxis]).ravel()
         _dpeak_pos = _dpeak_pos[(_dpeak_pos > 0.5) & (_dpeak_pos < 1.5)]
-        _h, _e = np.histogram(_dpeak_pos, bins=len(_peak_pos))
+        _h, _e = np.histogram(
+            _dpeak_pos, bins=len(_peak_pos), range=(0.5, 1.5)
+        )
         _c = (_e[1:] + _e[:-1]) / 2
-        _fit = FitGaussian1d(_c, _h)
-        spacings.append(_fit.x0)
+        try:
+            _fit = FitGaussian1d(_c, _h)
+            if not _fit.psuccess:
+                raise TypeError
+            spacings.append(_fit.x0)
+        except TypeError:
+            _pdf = _h / np.sum(_h)
+            spacings.append(np.sum(_c * _pdf))
         # Find peak width
         widths.append(np.mean(_peaks["width"]))
+        # Find fidelity (first peak should be around 1)
+        _min_peaks = np.sort(_peak_pos)[:2]
+        if (
+            (len(_min_peaks) < 2)
+            or (_min_peaks[0] < 1 - rel_spacing_tol
+                or _min_peaks[0] > 1 + rel_spacing_tol)
+            or (_min_peaks[1] < 2 - 2 * rel_spacing_tol
+                or _min_peaks[1] > 2 + 2 * rel_spacing_tol)
+        ):
+            fidelities.append(0)
+        else:
+            fidelities.append(1 - (widths[-1] / spacings[-1])**2)
     # Package result
     spacings, widths = np.array(spacings), np.array(widths)
-    fidelities = 1 - (widths / spacings)**2
+    fidelities = np.array(fidelities)
     # If high fidelity, perform precision spacing fit
     for i, fidelity in enumerate(fidelities):
         if fidelity > 0.5:
-            _fit = FitPeriodicGaussian1d(proj_dists[i])
+            _fit = FitPeriodicGaussian1d()
+            _fit.find_p0(proj_dists[i])
+            _fit.set_p0(x0=0, dx=spacings[i])
+            _fit.find_popt(proj_dists[i])
             if _fit.psuccess:
                 spacings[i] = _fit.dx
                 widths[i] = _fit.wx
-    fidelities = 1 - (widths / spacings)**2
+                fidelities[i] = 1 - (widths[i] / spacings[i])**2
+            else:
+                fidelities[i] = 0
     if ax_is_scalar:
         proj_dists, fidelities = proj_dists[0], fidelities[0]
         spacings, widths = spacings[0], widths[0]
@@ -111,10 +140,12 @@ def get_trafo_from_sites_direct(
     _t0 = time.time()
     test_fidelities = []
     test_rel_spacings = []
+    test_rel_widths = []
     for idx_trafo, idx_proj in enumerate(reversed(range(2))):
         # Iterate trafo test angles
         fidelities = []
         rel_spacings = []
+        rel_widths = []
         for idx_angle, test_angle in enumerate(test_angles[idx_trafo]):
             if print_progress:
                 misc.print_progress(
@@ -136,6 +167,7 @@ def get_trafo_from_sites_direct(
             )
             fidelities.append(_trafo_angle_fidelity["fidelity"])
             rel_spacings.append(_trafo_angle_fidelity["spacing"])
+            rel_widths.append(_trafo_angle_fidelity["width"])
         # Filter bad fidelities
         mask = np.array(fidelities) > min_fidelity
         fidelities = ArrayData(np.array(fidelities)[mask])
@@ -146,7 +178,14 @@ def get_trafo_from_sites_direct(
         )
         test_fidelities.append(fidelities)
         # Get fitted magnifications
-        test_rel_spacings.append(rel_spacings)
+        _ad = fidelities.copy()
+        _ad.data = np.array(rel_spacings)[mask]
+        _ad.set_data_quantity(name="relative spacing")
+        test_rel_spacings.append(_ad)
+        _ad = fidelities.copy()
+        _ad.data = np.array(rel_widths)[mask]
+        _ad.set_data_quantity(name="relative width")
+        test_rel_widths.append(_ad)
     if print_progress:
         misc.print_progress(2, 2, angle_num, angle_num, start_time=_t0)
     # Fit angles with best fidelities
@@ -165,7 +204,12 @@ def get_trafo_from_sites_direct(
                     test_fidelity, npeaks=1, rel_prominence=0,
                     fit_algorithm="mean"
                 )["center"]
-        angles.append(peaks[0])
+        try:
+            angles.append(peaks[0])
+        except IndexError:
+            raise RuntimeError(
+                "get_trafo_from_sites_direct failed. Try higher `angle_num`?"
+            )
     # Find spacing at best angles
     optimal_trafo = AffineTrafo2d()
     optimal_trafo.set_origin_axes(
@@ -182,7 +226,9 @@ def get_trafo_from_sites_direct(
     )
     return {
         "trafo": optimal_trafo,
-        "fidelity": test_fidelities
+        "fidelity": test_fidelities,
+        "rel_spacing": test_rel_spacings,
+        "rel_width": test_rel_widths
     }
 
 
@@ -209,6 +255,72 @@ def get_trafo_from_sites_fit(x, y, guess_trafo):
     popt_b = res.x[4:]
     trafo = AffineTrafo2d(matrix=popt_a, offset=popt_b)
     return trafo.invert()
+
+
+###############################################################################
+# Transformation estimator
+###############################################################################
+
+
+class TrafoEstimator:
+
+    LOGGER = get_logger("srec.TrafoEstimator")
+
+    def __init__(
+        self, isolated_locator=None, min_isolated_num=24,
+        guess_trafo=None, angle_num=16, angle_range=np.deg2rad(3),
+        bins_per_site=5, min_fidelity=0.8,
+        peak_rel_prominence=0.1, peak_base_prominence_ratio=0.1,
+    ):
+        # Isolated atom location estimator
+        self.isolated_locator = isolated_locator
+        self.min_isolated_num = min_isolated_num
+        # Trafo optimization: main configuration
+        self.guess_trafo = guess_trafo
+        self.angle_num = angle_num
+        self.angle_range = angle_range
+        # Trafo optimization: detailed configuration
+        self.bins_per_site = bins_per_site
+        self.min_fidelity = min_fidelity
+        self.peak_rel_prominence = peak_rel_prominence
+        self.peak_base_prominence_ratio = peak_base_prominence_ratio
+
+    def check_setup(self):
+        if self.isolated_locator is None:
+            raise RuntimeError("invalid `isolated_locator`")
+        if self.guess_trafo is None:
+            raise RuntimeError("invalid `guess_trafo`")
+        return True
+
+    def get_optimized_trafo(self, image, print_progress=False):
+        self.check_setup()
+        # Find atom centers
+        label_centers = self.isolated_locator.get_label_centers(image)
+        if len(label_centers[..., 0]) < self.min_isolated_num:
+            return False
+        # Optimize trafo
+        trafo_params = get_trafo_from_sites_direct(
+            *np.transpose(label_centers), self.guess_trafo,
+            print_progress=print_progress
+        )
+        opt_trafo = trafo_params["trafo"]
+        return opt_trafo
+
+    def find_trafo(self, *images, print_progress=False):
+        _iter = misc.iter_progress(images) if print_progress else images
+        opt_trafos = []
+        for i, im in enumerate(_iter):
+            _trafo = self.get_optimized_trafo(im)
+            if _trafo is False:
+                self.LOGGER.warn(f"Too few isolated atoms in image[{i:d}]")
+            else:
+                opt_trafos.append(_trafo)
+        if len(opt_trafos) == 0:
+            raise RuntimeError("No suitable image found")
+        _matrix = np.mean([_trafo.matrix for _trafo in opt_trafos], axis=0)
+        _offset = np.mean([_trafo.offset for _trafo in opt_trafos], axis=0)
+        trafo = AffineTrafo2d(matrix=_matrix, offset=_offset)
+        return trafo
 
 
 ###############################################################################
