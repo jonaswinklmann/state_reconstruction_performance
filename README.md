c533348c3500b8519facaeef201094cbe92836af
David Wei <david94.wei@gmail.com>
2022-05-20 15:06:02 +0200

    gen.trafo_gen: Remove librbl dependency

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# state_reconstruction
+# State reconstruction
+
+Quantum state reconstruction from microscopic fluorescence images of atoms on a lattice.
+
+Includes the following functionalities:
+* Simulate noisy fluorescence images
+* Extract subpixel point spread functions
+* Extract lattice vectors
+* Reconstruct single-site occupation
+
+
+## Installation
+
+Clone this repository:
+
+```
+git clone https://www.github.com/david-wei/state_reconstruction
+```
+
+
+Install the library:
+
+```
+pip install ./state_reconstruction
+```
+
+
+## Dependencies
+
+* [libics](https://www.github.com/david-wei/libics)-v1
+
+
+## Getting started
+
+...
--- state_reconstruction/gen/trafo_gen.py
+++ state_reconstruction/gen/trafo_gen.py
@@ -1,17 +1,73 @@
 import numpy as np
 
-from librbl.seq.data import get_trafo_site_to_fluo
+from libics.tools.trafo.linear import AffineTrafo2d
 
 
-def get_trafo_site_to_image(
-    angle=np.deg2rad((0, 0)),
-    relative_magnification=1
+###############################################################################
+
+
+def get_trafo_site_to_fluo(
+    magnification=None, angle=None, trafo_site_to_image=None,
+    phase_ref_image=(255, 255), phase_ref_site=(84, 84), phase=np.zeros(2)
 ):
-    _m = np.mean(get_trafo_site_to_fluo().get_origin_axes()[0])
-    gen_trafo_site_to_fluo = get_trafo_site_to_fluo(
-        magnification=np.full(2, _m*relative_magnification),
-        angle=angle,
-        phase_ref_site=(84, 84),
-        phase_ref_fluo=(255, 255)
-    )
-    return gen_trafo_site_to_fluo
+    """
+    Gets the default transformation.
+
+    Parameters
+    ----------
+    magnification, angle : `np.ndarray(1, float)` or `None`
+        Origin axes magnification and angle.
+    trafo_site_to_image : `AffineTrafo2d` or `None`
+        Uses the origin axes from this trafo object.
+        Is overwritten by `magnification` and `angle`.
+    phase_ref_image, phase_ref_site : `np.ndarray(1, float)` or `None`
+        Phase reference (phase = (0, 0)) in fluorescence image
+        and lattice site coordinates.
+    phase : `np.ndarray(float, float)`
+        Lattice phase (fluo axes in site space = reference + phase).
+
+    Returns
+    -------
+    trafo : `AffineTrafo2d`
+        Site-to-fluorescence-image transformation.
+    """
+    # Parse parameters
+    if trafo_site_to_image is not None:
+        _m, _a, _ = trafo_site_to_image.get_origin_axes()
+    if magnification is None:
+        magnification = _m
+    if angle is None:
+        angle = _a
+    # Setup trafo
+    trafo = AffineTrafo2d()
+    trafo.set_origin_axes(magnification=magnification, angle=angle)
+    _a, _b, _ = trafo.get_target_axes()
+    _offset = phase_ref_site - trafo.coord_to_origin(phase_ref_image)
+    trafo.set_target_axes(magnification=_a, angle=_b, offset=_offset + phase)
+    return trafo
+
+
+def get_phase_from_trafo_site_to_image(
+    trafo_site_to_image, phase_ref_image=(255, 255)
+):
+    """
+    Gets the lattice phase and ref. integer site in `librbl` convention.
+
+    Parameters
+    ----------
+    trafo_site_to_image : `AffineTrafo2d`
+        Transformation between lattice sites and fluorescence image.
+    phase_ref_fluo : `np.ndarray(1, float)`
+        Fluorescence image coordinate corresponding to integer lattice site.
+
+    Returns
+    -------
+    phase : `np.ndarray(1, float)`
+        Phase (residual) of lattice w.r.t. to image coordinates (0, 0).
+    site : `np.ndarray(1, float)`
+        Nearest integer lattice site.
+    """
+    site_float = trafo_site_to_image.coord_to_origin(phase_ref_image)
+    phase = (site_float + 0.5) % 1 - 0.5
+    site = np.round(site_float)
+    return phase, site
