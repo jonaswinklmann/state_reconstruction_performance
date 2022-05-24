78ef95fb23ae4ddb504a3cb67eeadea97d6f11c6
David Wei <david94.wei@gmail.com>
2022-05-24 18:40:18 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(name='state_reconstruction',
             'gui_scripts': []
       },
       install_requires=[
+            'numba', 'numpy', 'scipy',
       ],
       python_requires='>=3.6',
       dependency_links=[
